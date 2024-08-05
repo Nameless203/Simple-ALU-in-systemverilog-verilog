85343a4fbfa2d5d4ddd9ff38ebc28ccee68dec4e
Nameless203 <43174452+Nameless203@users.noreply.github.com>
2024-08-05 15:49:41 +0000

    Add files via upload
    full-adder 1bit add
    full-subtract 1bit add
    subtract 1bit add
    adder 1bit add

---
+++ full_adder_1b.v
@@ -0,0 +1,13 @@
+module full_adder_1b (i_carry,i_bit1,i_bit2,o_carry,o_sum);
+
+input i_carry;
+input i_bit1;
+input i_bit2;
+output o_carry;
+output o_sum;
+
+assign o_sum = i_bit1 ^ i_bit2 ^ i_carry;
+assign o_carry = ((i_bit1 ^ i_bit2) & i_carry) | (i_bit1 & i_bit2);
+
+endmodule
+
---
+++ sub_1b.v
@@ -0,0 +1,7 @@
+module sub_1b (i_bit1,i_bit2,o_sub);
+input i_bit1,i_bit2;
+output o_sub;
+
+assign o_sub = i_borrow ^ i_bit1 ^ i_bit2;
+
+endmodule
